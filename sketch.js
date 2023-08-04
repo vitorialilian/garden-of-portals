9c150ce8212d51708efe3f5b1f5869ae140d214d
Vitória Lilian <127898471+vitorialilian@users.noreply.github.com>
2023-08-04 15:30:58 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,5 +1,9 @@
 let personagem;
-let grama;
+let portais;
+let portal;
+let portal2;
+let turma;
+let font;
 const tamanho = 64;
 
 let andarX = 0;
@@ -10,44 +14,54 @@ let botao;
 //executando apenas uma vez ao iniciar o programa
 function setup() {
   createCanvas(576, 576);
-  personagem = loadImage('rick.png');
-  grama = loadImage('grass.jpg');
+  personagem = loadImage('rickemorty.png');
+  portais = loadImage('portal.png');
+  portal = loadImage('portal.png');
+  portal2 = loadImage('portal2.png');
+  turma = loadImage('rickandmorty2.png');
+  font = loadFont('JotiOne-Regular.ttf');
 }
 
 //fica executando até que o programa se encerre
 function draw() {
   background(228);
-  
-  if(andarX < 0) {
+
+  if (andarX < 0) {
     andarX = 0
-  } 
-  
-  if(andarY < 0) {
+  }
+
+  if (andarY < 0) {
     andarY = 0
   }
-  
-  if(andarX > tamanho * 8) {
+
+  if (andarX > tamanho * 8) {
     andarX = tamanho * 8
   }
-  
-  if(andarY > tamanho * 8) {
+
+  if (andarY > tamanho * 8) {
     andarY = tamanho * 8
   }
-  
-  for(let x = 0; x < 9; x++){
-     for(let y = 0; y < 9; y++) {
-    image(grama, x * tamanho, y * tamanho, tamanho, tamanho);
+
+  for (let x = 0; x < 9; x++) {
+    for (let y = 0; y < 9; y++) {
+      image(portais, x * tamanho, y * tamanho, tamanho, tamanho);
     }
   }
-  
+
+  image(portal, tamanho * 8, tamanho * 8, tamanho, tamanho);
+
   image(personagem, andarX, andarY, tamanho, tamanho);
-  
-  if(andarX === tamanho * 8 && andarY === tamanho * 8) {
+
+  if (andarX === tamanho * 8 && andarY === tamanho * 8) {
     // criar retangulo
-    rect(160, 160, 256, 256)
+    rect(0, 0, 576, 576)
     // escrever o texto ganhou 
-    textSize(35)
-    text('Ganhou!', 200, 300)
+    textSize(100)
+    textFont(font)
+    text('Você', 150, 150)
+    text('Venceu!', 100, 250)
+    image(turma, 170, 384, 400, 200)
+    image(portal2, 0, 400, 180, 180)
     // criar um botão
     botao = createButton('Reiniciar')
     // resetar o jogo
@@ -57,7 +71,7 @@ function draw() {
   }
 }
 
-function reset(){
+function reset() {
   andarX = 0
   andarY = 0
   botao.remove()
@@ -67,19 +81,19 @@ function reset(){
 //é chamada uma vez toda vez que uma tecla é pressionada
 function keyPressed() {
   // DOWN_ARROW, UP_ARROW, RIGHT_ARROW, LEFT_ARROW
-  if(keyIsDown(DOWN_ARROW)){
+  if (keyIsDown(DOWN_ARROW)) {
     andarY += 64
   }
-  
-  if(keyIsDown(UP_ARROW)){
+
+  if (keyIsDown(UP_ARROW)) {
     andarY -= 64
   }
-  
-  if(keyIsDown(RIGHT_ARROW)){
+
+  if (keyIsDown(RIGHT_ARROW)) {
     andarX += 64
   }
-  
-  if(keyIsDown(LEFT_ARROW)){
+
+  if (keyIsDown(LEFT_ARROW)) {
     andarX -= 64
   }
 }
